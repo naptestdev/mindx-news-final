ce7ef9976971284c722e5cdada23dba397f3c2cd
napthedev <phongna.dev@gmail.com>
2023-09-17 21:07:45 +0700

    feat: navbar dropdowns and fix navbar on mobile devices

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 0)
+    document.querySelector(".back-to-top").classList.add("back-to-top-visible");
+  else
+    document
+      .querySelector(".back-to-top")
+      .classList.remove("back-to-top-visible");
+});
+
+document
+  .querySelector(".back-to-top")
+  .addEventListener("click", () =>
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  );
+
+document.querySelector("#hamburger-button").addEventListener("click", () => {
+  document
+    .querySelector(".navbar-side-right")
+    .classList.toggle("navbar-side-right-visible");
+});
+
 document.querySelector("#search-button").addEventListener("click", () => {
   document.querySelector(".search-box").classList.add("search-box-visible");
   document.querySelector("#search-button").style.background = "white";
